e10a5f0496c17e7169d24f1670d8791ea7abb07b
shamim5232 <shamimreja.webdesign@gmail.cm>
2024-04-09 17:14:23 +0600

    real estate property added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/SharedPages/Navbar/Navbar";
+import Footer from "../Pages/SharedPages/Footer/Footer";
 
 const Root = () => {
   return (
-    <div className="font-popins w-full px-5 lg:px-0 lg:max-w-[95%] mx-auto">
-      <Navbar></Navbar>
-      <Outlet></Outlet>
+    <div>
+      <div className="font-popins w-full px-5 lg:px-0 lg:max-w-[95%] mx-auto">
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
